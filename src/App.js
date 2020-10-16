121a01e9889ef6ce0f90b62058348d66e0a82dd4
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-15 18:06:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
+
 
 function App() {
   return (
-    // eslint-disable-next-line react/jsx-no-undef
+    
     <Container>
-      <Button variant="contained" color="primary">Here is the Button</Button>
-      <Button variant="contained" color="primary">Here is the Button</Button>
-      <Button variant="contained" color="primary">Here is the Button</Button>
-      <Button variant="contained" color="primary">Here is the Button</Button>
-      <Button variant="contained" color="primary">Here is the Button</Button>
-      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button>This is a Button </Button>
     </Container>
+    
+   
   );
 }
 
