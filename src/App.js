872003f9147073a0bbb329dbabe9b8d85d6903af
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-15 17:44:57 -0700

    Build out Homepage
    Going to build out the structure of the website then work on the functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 function App() {
   return (
-   <div>Here is the content</div>
+    // eslint-disable-next-line react/jsx-no-undef
+    <Container>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+      <Button variant="contained" color="primary">Here is the Button</Button>
+    </Container>
   );
 }
 
