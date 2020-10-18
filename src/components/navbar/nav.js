8bb1ce60dd4532fd35b480637f893c77052a498d
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-17 20:53:19 -0700

    Update nav.js

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -3,7 +3,9 @@ import React from 'react'
 function nav() {
     return (
         <div>
-            <p>Nav Bar Area - Home - About Us - Contact Us - Login / Sign up </p>
+            <p>Nav Bar Area - Home - About Us - Contact Us</p>
+            <button>Login/Signup</button>
+            <button>New Listing (when signed in)</button>
         </div>
     )
 }
