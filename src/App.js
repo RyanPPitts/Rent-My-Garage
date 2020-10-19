d0993b7d33aefcda8f5cc7599c0a1dcfef30b178
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-18 17:40:30 -0700

    Building routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
+
 import Navbar from './components/navbar/nav';
-import Hero from './components/hero/hero';
-import Footer from './components/footer/footer';
+
+
 import About from './components/About/About';
 import Homepage from './components/Homepage/Homepage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -15,19 +15,12 @@ function App() {
   return (
     <Router>
     <Container>
-     
       <Navbar/>
       <Switch>
       <Route path='/' component={Homepage}/>
       <Route path='/about' component={About}/>
-
       </Switch>
-    
-      <Hero/>
-      <Button>This is a Button </Button>
-      <Footer/>
     </Container>
-
     </Router>
   );
 }
