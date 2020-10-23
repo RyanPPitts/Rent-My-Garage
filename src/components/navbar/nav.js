f9ce42cb6c02b7f0a602bd5f18bf8193aa1bb465
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-22 19:55:10 -0700

    Creating nav routes

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function nav() {
+    const navStyle = {
+        color: 'white'
+    };
+
     return (
         <nav>
             <h3>Logo</h3>
             <ul className="nav-links">
+                <Link style={navStyle} to='/about'>
                 <li>About</li>
+                </Link>
+                <Link style={navStyle} to='/contactus'>
+
                 <li>Contact Us</li>
+                </Link>
+                
                 <li>Sign Up</li>
                 <li>Login</li>
             </ul>
