0929be3c94304a82b9d07ca9cea5d475c7baf41e
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-22 20:51:57 -0700

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,4 +8,7 @@ function Login() {
     )
 }
 
+
+
+
 export default Login
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -8,4 +8,9 @@ function Signup() {
     )
 }
 
+// Need to setup database to capture sign up information
+// Create form for sign up information
+//  information transferred to database successfully
+// once login is completed the user is sent to the login page to log into account page
+
 export default Signup
