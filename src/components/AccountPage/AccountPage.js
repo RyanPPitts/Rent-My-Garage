b825a27f55a7063df2b8fd65c461cbf31eac6b37
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-22 20:51:00 -0700

    update

---
+++ src/components/AccountPage/AccountPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AccountPage() {
+    return (
+        <div>
+            <p>This is the account page. This is where the customer will see their information</p>
+            <p> This page will also allow the user to create a listing for their garage</p>
+        </div>
+    )
+}
+
+export default AccountPage
---
+++ src/components/ListingPage/ListingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListingPage() {
+    return (
+        <div>
+            <p>This is the listing page. This is where the user can upload a new listing for rent</p>
+        </div>
+    )
+}
+
+export default ListingPage
